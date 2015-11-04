11742717a4c1afe83f85c348fe70796b7fdbaaad
rodr <ohrodr@gmail.com>
2015-11-04 09:29:39 -0800

    fixing readme and example binary

--- gramgen.py
+++ gramgen.py
@@ -1,39 +1,37 @@
+import argparse
+import fileinput
+import locale
+import os, sys
+
+from gramcracker import GrammarWorker
+from gramcracker import GrammarManager
 
 if __name__ == '__main__':
-    import argparse
-    import fileinput
-    import locale
-    import os, sys
+  reload(sys)
+  sys.setdefaultencoding("utf_8")
+  locale.setlocale(locale.LC_ALL,"")
 
-    from gramcracker import GrammarWorker
-    from gramcracker import GrammarManager
+  parser = argparse.ArgumentParser(
+   description='This will create appropriate grammar files for cracking use')
+  parser.add_argument('--verbose', '-v', action='count')
+  parser.add_argument('--session', metavar='session_name', default='gramcracker', help='Session Name')
+  parser.add_argument('--filename', '-f', metavar='pwlist', help='file of listed passwords')
+  args = parser.parse_args()
+  g = GrammarWorker(args.session)
 
-    reload(sys)
-    sys.setdefaultencoding("utf_8")
-    locale.setlocale(locale.LC_ALL,"")
+  # send some passwords
+  if not os.path.isfile(args.filename):
+    print "Please provide a valid password file."
+    sys.exit(1)
 
-    parser = argparse.ArgumentParser(
-     description='This will create appropriate grammar files for cracking use')
-    parser.add_argument('--verbose', '-v', action='count')
-    parser.add_argument('--session', metavar='session_name', default='gramcracker', help='Session Name')
-    parser.add_argument('--filename', '-f', metavar='pwlist', help='file of listed passwords')
-    args = parser.parse_args()
-    g = GrammarWorker(args.session)
-
-    # send some passwords
-    if not os.path.isfile(args.filename):
-      print "Please provide a valid password file."
-      sys.exit(1)
-
-    with open(args.filename) as fh:
-      for line in fh.readlines():
-        g.parse(line.rstrip())
-    # parse the grammar
-    grammar = GrammarManager(worker=g)
-    grammar.calculate()
-    # define some verbosity
-    if args.verbose:
-        print grammar.grammar
-        print grammar.digits
-        print grammar.specials
-
+  with open(args.filename) as fh:
+    for line in fh.readlines():
+      g.parse(line.rstrip())
+  # parse the grammar
+  grammar = GrammarManager(worker=g)
+  grammar.calculate()
+  # define some verbosity
+  if args.verbose:
+    print grammar.grammar
+    print grammar.digits
+    print grammar.specials
