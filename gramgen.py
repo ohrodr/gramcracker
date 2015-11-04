0bb8f9948befabf332813933c1496fe000716990
rodr <ohrodr@gmail.com>
2015-11-04 08:43:51 -0800

    adding grammar generator

--- gramgen.py
+++ gramgen.py
@@ -1,11 +1,13 @@
 
 if __name__ == '__main__':
     import argparse
-    import sys
+    import fileinput
     import locale
+    import os, sys
 
     from gramcracker import GrammarWorker
     from gramcracker import GrammarManager
+
     reload(sys)
     sys.setdefaultencoding("utf_8")
     locale.setlocale(locale.LC_ALL,"")
@@ -13,15 +15,18 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
      description='This will create appropriate grammar files for cracking use')
     parser.add_argument('--verbose', '-v', action='count')
-    parser.add_argument('--session',metavar='session_name',help='Session Name')
-    parser.add_argument('--filename','-f',metavar='pwlist',help='file of listed passwords')
+    parser.add_argument('--session', metavar='session_name', default='gramcracker', help='Session Name')
+    parser.add_argument('--filename', '-f', metavar='pwlist', help='file of listed passwords')
     args = parser.parse_args()
     g = GrammarWorker(args.session)
 
     # send some passwords
+    if not os.path.isfile(args.filename):
+      print "Please provide a valid password file."
+      sys.exit(1)
 
-    import fileinput
-    for line in fileinput.input([args.filename]):
+    with open(args.filename) as fh:
+      for line in fh.readlines():
         g.parse(line.rstrip())
     # parse the grammar
     grammar = GrammarManager(worker=g)
