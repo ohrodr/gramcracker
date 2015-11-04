69a862d79c39c739593aef6c365d3314f76e9b4c
rodr <ohrodr@gmail.com>
2015-11-04 08:30:17 -0800

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='GramCracker',
+    version='0.2',
+    packages=['gramcracker',],
+    license='Apache License 2.0',
+    long_description=open('README').read(),
+)
